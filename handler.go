75b668fd85584bc0a6dee59ea4df85ff3b0cf7c8
interrrp <int@addictedto.coffee>
2024-02-11 15:27:05 +0800

    Create basic proxy

--- handler.go
+++ handler.go
@@ -1,7 +1,29 @@
 package main
 
-import "net"
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net"
+)
 
 func handleClient(client net.Conn) {
 	defer client.Close()
+
+	if rand.Intn(100) < chance {
+		log.Printf("disconnecting %s", client.RemoteAddr())
+		return
+	}
+
+	log.Printf("proxying %s", client.RemoteAddr())
+
+	svr, err := net.Dial("tcp", target)
+	if err != nil {
+		log.Printf("error dialing server: %v", err)
+		return
+	}
+	defer svr.Close()
+
+	go io.Copy(svr, client)
+	io.Copy(client, svr)
 }
