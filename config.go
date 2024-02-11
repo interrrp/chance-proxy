21e794b014e17ae3b449469a91e8f984f9eaadde
interrrp <int@addictedto.coffee>
2024-02-11 15:35:51 +0800

    Fix flags not being set

--- config.go
+++ config.go
@@ -3,11 +3,14 @@ package main
 import "flag"
 
 var (
-	address = *flag.String("address", "localhost:8081", "Address to listen on")
-	target  = *flag.String("target", "localhost:8080", "Target address")
-	chance  = *flag.Int("chance", 20, "Chance of failure")
+	address string
+	target  string
+	chance  int
 )
 
 func init() {
+	flag.StringVar(&address, "address", "localhost:8081", "Address to listen on")
+	flag.StringVar(&target, "target", "localhost:8080", "Target address")
+	flag.IntVar(&chance, "chance", 20, "Chance of failure")
 	flag.Parse()
 }
